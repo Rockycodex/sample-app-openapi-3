8e2186a51956cf0723ef1a0610040d093c6ed37f
Nidhi Verma <vnidhi235@gmail.com>
2023-03-02 00:01:53 +0530

    Swagger 2 setup

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const fs = require("fs")
+const YAML = require('yaml');
+const swaggerUi = require('swagger-ui-express');
+const file  = fs.readFileSync('./api-spec.yml', 'utf8')
+const swaggerDocument = YAML.parse(file)
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -12,6 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
